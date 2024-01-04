1abe9b04f5a241b315b6ca634956143038105ea5
Jehmni <jehmnitech@gmail.com>
2024-01-04 09:54:47 +0000

    Add project files.

---
+++ Graph.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace stringGraph
+{
+    public class Graph
+    {
+        int numOfEdges;
+        // A list that will hold the nodes in the graph
+        private LinkedList<GraphNode> nodes;
+
+        // Constructor to set the list of nodes to an empty list
+        public Graph()
+        {
+            nodes = new LinkedList<GraphNode>();
+        }
+
+        // Adds a new node to the graph with the specified ID
+        public void AddNode(String id)
+        {
+            nodes.AddLast(new GraphNode(id));
+        }
+
+        // Returns the node with the specified ID
+        public GraphNode GetNode(String id)
+        {
+            foreach (GraphNode node in nodes)
+            {
+                if (id.CompareTo(node.ID) == 0)
+                    return node;
+            }
+
+            return null;
+        }
+
+        // Adds a directed edge between two nodes specified by their IDs
+        public void AddEdge(string from, string to)
+        {
+            // Get the GraphNode that corresponds to the node with ID "from"
+            GraphNode node1 = GetNode(from);
+
+            // Get the GraphNode that corresponds to the node with ID "to"
+            GraphNode node2 = GetNode(to);
+
+            // Add a directed edge from node1 to node2
+            node1.AddEdge(node2);
+
+            numOfEdges++;
+        }
+
+        // Checks if the graph is empty (has no nodes)
+        public bool IsEmptyGraph()
+        {
+            // Returns true if the number of nodes in the list is 0, false otherwise
+            return nodes.Count == 0;
+        }
+
+        // Returns the total number of nodes in the graph
+        public int NumberOfNodes()
+        {
+            return nodes.Count;
+        }
+
+        // Returns the total number of edges present in the graph
+        public int NumEdgesGraph()
+        {
+            return numOfEdges;
+        }
+
+        // Displays the IDs of nodes adjacent to the node with the specified ID
+        public void DisplayAdjNodes(string inputID)
+        {
+            // Define a list to store the adjacency list of the node
+            LinkedList<string> adjl = new LinkedList<string>();
+
+            // Get the GraphNode of the node with the specified ID
+            GraphNode n = GetNode(inputID);
+
+            // Get the adjacency list of node n and store it in adjl
+            adjl = n.GetAdjList();
+
+            // Display the IDs of all nodes in adjl
+            foreach (string id in adjl)
+            {
+                Console.WriteLine(id);
+            }
+        }
+
+        // Checks if the graph contains a node with the specified ID
+        public bool ContainsGraph(string ID)
+        {
+            foreach (GraphNode n in nodes)
+            {
+                // Check if the ID of the current node is equal to the specified ID
+                if (n.ID.CompareTo(ID) == 0)
+                    return true;
+            }
+
+            // Node with the specified ID was not found, so return false
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringGraph
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the Graph
+            Graph nigeria = new Graph();
+
+            // Add nodes in the graph
+            nigeria.AddNode("Abia");
+            nigeria.AddNode("Adamawa");
+            nigeria.AddNode("Akwa Ibom");
+            nigeria.AddNode("Anambra");
+            nigeria.AddNode("Bauchi");
+            nigeria.AddNode("Bayelsa");
+            nigeria.AddNode("Benue");
+            nigeria.AddNode("Borno");
+            nigeria.AddNode("Cross River");
+            nigeria.AddNode("Delta");
+            nigeria.AddNode("Ebonyi");
+            nigeria.AddNode("Edo");
+            nigeria.AddNode("Ekiti");
+            nigeria.AddNode("Enugu");
+            nigeria.AddNode("Gombe");
+            nigeria.AddNode("Imo");
+            nigeria.AddNode("Jigawa");
+            nigeria.AddNode("Kaduna");
+            nigeria.AddNode("Kano");
+            nigeria.AddNode("Katsina");
+            nigeria.AddNode("Kebbi");
+            nigeria.AddNode("Kogi");
+            nigeria.AddNode("Kwara");
+            nigeria.AddNode("Lagos");
+            nigeria.AddNode("Nasarawa");
+            nigeria.AddNode("Niger");
+            nigeria.AddNode("Ogun");
+            nigeria.AddNode("Ondo");
+            nigeria.AddNode("Osun");
+            nigeria.AddNode("Oyo");
+            nigeria.AddNode("Plateau");
+            nigeria.AddNode("Rivers");
+            nigeria.AddNode("Sokoto");
+            nigeria.AddNode("Taraba");
+            nigeria.AddNode("Yobe");
+            nigeria.AddNode("Zamfara");
+            nigeria.AddNode("Abuja");
+
+            // Add connections between the nodes. (Directed edges)
+            // These connections represent the adjacency between the states in Nigeria
+            nigeria.AddEdge("Abia", "Akwa Ibom");
+            nigeria.AddEdge("Abia", "Imo");
+            nigeria.AddEdge("Adamawa", "Borno");
+            nigeria.AddEdge("Adamawa", "Taraba");
+            nigeria.AddEdge("Adamawa", "Yobe");
+            nigeria.AddEdge("Akwa Ibom", "Cross River");
+            nigeria.AddEdge("Akwa Ibom", "Delta");
+            nigeria.AddEdge("Anambra", "Delta");
+            nigeria.AddEdge("Anambra", "Enugu");
+            nigeria.AddEdge("Anambra", "Kogi");
+            nigeria.AddEdge("Bauchi", "Gombe");
+            nigeria.AddEdge("Bauchi", "Jigawa");
+            nigeria.AddEdge("Bauchi", "Kaduna");
+            nigeria.AddEdge("Bauchi", "Kano");
+            nigeria.AddEdge("Bauchi", "Plateau");
+            nigeria.AddEdge("Bayelsa", "Delta");
+            nigeria.AddEdge("Bayelsa", "Rivers");
+            nigeria.AddEdge("Benue", "Kogi");
+            nigeria.AddEdge("Benue", "Nasarawa");
+            nigeria.AddEdge("Benue", "Taraba");
+            nigeria.AddEdge("Borno", "Yobe");
+            nigeria.AddEdge("Cross River", "Ebonyi");
+            nigeria.AddEdge("Cross River", "Enugu");
+            nigeria.AddEdge("Cross River", "Benue");
+            nigeria.AddEdge("Delta", "Edo");
+            nigeria.AddEdge("Delta", "Kogi");
+            nigeria.AddEdge("Delta", "Ondo");
+            nigeria.AddEdge("Delta", "Rivers");
+            nigeria.AddEdge("Ebonyi", "Enugu");
+
+            // Test the functionality of the Graph class
+            Console.WriteLine("Graph empty? True or False");
+            Console.WriteLine(nigeria.IsEmptyGraph());
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("This is to test the Contains method");
+            Console.WriteLine("Yes, graph contains state called " + nigeria.GetNode("Akwa Ibom").ID);
+            Console.WriteLine("Does graph contain state called 'Abia', True or False?");
+            Console.WriteLine(nigeria.ContainsGraph("Abia"));
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Display all the states you can get to from 'Bauchi'");
+            nigeria.DisplayAdjNodes("Bauchi");
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Total Number of states is:" + nigeria.NumberOfNodes());
+            Console.WriteLine("Total Number of edges is:" + nigeria.NumEdgesGraph());
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
